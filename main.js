12afd863a4b8007dd91d1859913474e5ac23890b
erum abbas <eruma6843@gmail.com>
2024-05-09 16:11:13 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+let auther = "albert Einstein";
+let quote = "A person who never make a mistake never tried anything new.";
+console.log(`${auther} once said, ${quote}`);
---
+++ main.ts
@@ -0,0 +1,3 @@
+let auther: string = "albert Einstein";
+let quote: string ="A person who never make a mistake never tried anything new."
+console.log(`${auther} once said, ${quote}`)
